efa4716b5c0bbeba019c01fe458cbab4cff589a2
Anastasiya <chufa4ka@gmail.com>
2022-11-06 15:37:53 +0100

    Task2 completed

--- src/main/java/com/nphase/service/ShoppingCartService.java
+++ src/main/java/com/nphase/service/ShoppingCartService.java
@@ -1,8 +1,8 @@
 package com.nphase.service;
 
+import com.nphase.entity.Product;
 import com.nphase.entity.ShoppingCart;
 
-import javax.naming.OperationNotSupportedException;
 import java.math.BigDecimal;
 
 public class ShoppingCartService {
@@ -10,8 +10,23 @@ public class ShoppingCartService {
     public BigDecimal calculateTotalPrice(ShoppingCart shoppingCart) {
         return shoppingCart.getProducts()
                 .stream()
-                .map(product -> product.getPricePerUnit().multiply(BigDecimal.valueOf(product.getQuantity())))
+                .map(this::doCalculations)
                 .reduce(BigDecimal::add)
                 .orElse(BigDecimal.ZERO);
+    }
+
+    private BigDecimal doCalculations(Product product) {
+        final var quantity = product.getQuantity();
+        final var payment = product.getPricePerUnit().multiply(BigDecimal.valueOf(quantity));
+
+        return applyDiscount(payment, quantity);
+    }
+
+    private BigDecimal applyDiscount(BigDecimal payment, Integer quantity){
+        if(quantity > 3) {
+            return payment.multiply(BigDecimal.valueOf(0.9));
+        }
+
+        return payment;
     }
 }
